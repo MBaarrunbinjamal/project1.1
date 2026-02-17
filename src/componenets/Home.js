40965f5ebc094db3ab0ebedec60aacfb9d36886b
Baarrun <mbaarrun2008@gmail.com>
2026-02-17 10:52:42 +0500

    Baarun 2026

--- src/componenets/Home.js
+++ src/componenets/Home.js
@@ -7,9 +7,22 @@ import Blog from "./Blog";
 function Home () {
     return (
        <>
+          <div className="product-section">
+			<div className="container">
+				<div className="row">
 
-       
-        <Product/>
+				
+					<div className="col-md-12 col-lg-3 mb-5 mb-lg-0">
+						<h2 className="mb-4 section-title">Crafted with excellent material.</h2>
+						<p className="mb-4">Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique. </p>
+						<p><a href="shop.html" className="btn">Explore</a></p>
+					</div> 
+        <Product Productname="Nordic Chair" Productprice="$50.00" Productimage="images/product-1.png" />
+              <Product Productname="Kruzo Aero Chair" Productprice="$78.00" Productimage="images/product-2.png" />
+                    <Product Productname="Ergonomic Chair" Productprice="$43.00" Productimage="images/product-3.png" />
+        	</div>
+			</div>
+		</div>
         <Choose/>
         <Help/>
         <Popular/>
